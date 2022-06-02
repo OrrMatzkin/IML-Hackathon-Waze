595aeec6dbecdffaa26edd0b37c5805631fbfdeb
OrrMatzkin <orr.matzkin@gmail.com>
2022-06-03 00:09:58 +0300

    got f1_score

--- fit_kit.py
+++ fit_kit.py
@@ -1,23 +1,52 @@
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import ExtraTreesClassifier
 
-from main import cross_validation
+types = {'ACCIDENT': 0, 'JAM': 1, 'ROAD_CLOSED': 2, "WEATHERHAZARD": 3}
+
+def cross_validation(estimator, X_train, y_train, k_range):
+    param_grid = {'n_neighbors': k_range}
+    knn_cv = GridSearchCV(estimator(), param_grid, cv=5, scoring='f1_micro').fit(X_train, y_train)
+    cv_errors = 1 - knn_cv.cv_results_["mean_test_score"]
+    # std = knn_cv.cv_results_["std_test_score"]
+
+    min_ind = np.argmin(np.array(cv_errors))
+    selected_k = np.array(k_range)[min_ind]
+    selected_error = cv_errors[min_ind]
+
+    return selected_k, selected_error
 
 
-def fit_types_and_subtypes(X_train, y_train):
-    k = cross_validation(KNeighborsClassifier, X_train, y_train, np.linspace(1, 20, 20))[0]
-    model_types = get_knn_model(X_train, y_train, k)
+def get_knn_model(X_train: np.ndarray, y_train: np.ndarray, k: int) -> KNeighborsClassifier:
+    model = KNeighborsClassifier(n_neighbors=k)
+    model.fit(X_train, y_train)
+    return model
 
+
+def fit_types_and_subtypes(X_train, y_train_type, y_train_subtype):
+    print("here1")
+    k = cross_validation(KNeighborsClassifier, X_train, y_train_type, np.linspace(1, 20, 20).astype(int))[0]
+    model_types = get_knn_model(X_train, y_train_type, k)
+
+    y_predict = model_types.predict(X_train)
+
+    print("here2")
     model_sub_types = []
-    for i in range(4):
-        msk = X_train.loc[:, ['linqmap_type']] == i
+    for type_name in types.keys():
+        print(type_name)
+        msk = np.where(y_predict == type_name, True, False)
+        print("3")
         new_X_train = X_train[msk]
-        new_y_train = y_train[msk]
+        new_y_train = y_train_subtype[msk]
+        print('5')
+        if new_X_train.empty:
+            continue
+        k = cross_validation(KNeighborsClassifier, new_X_train, new_y_train, np.linspace(1, 20, 20).astype(int))[0]
+        model = get_knn_model(new_X_train, new_y_train, k)
+        model_sub_types.append((type_name, model, model.predict(new_X_train), new_y_train))
+    return model_types, y_predict, model_sub_types
 
-        k = cross_validation(KNeighborsClassifier, new_X_train, new_y_train, np.linspace(1, 20, 20))[0]
-        model_sub_types.append(get_knn_model(new_X_train, new_y_train, k))
-
-    return model_types, model_sub_types
 
 def predict_type_and_subtype(real_test, model_types, model_sub_types):
     types_pred = model_types.predict(real_test)
--- main.py
+++ main.py
@@ -7,47 +7,19 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 import plotly.graph_objects as go
-from sklearn.neighbors import KNeighborsRegressor
-
-from evaluate_x_y import check_evaluate_x_y
-from model_selection import model_selection
 from preprocess import *
-
+from fit_kit import *
 
 
 def cross_validation(estimator, X_train, y_train, X_test, y_test, k_range):
-    k_range = np.linspace(1, 20, 20).astype(int)
-
-    train_errors, test_errors = [], []
-    for k in k_range:
-        model = KNeighborsClassifier(k).fit(X_train, y_train)
-        train_errors.append(1 - model.score(X_train, y_train))
-        test_errors.append(1 - model.score(X_test, y_test))
-
     param_grid = {'n_neighbors': k_range}
     knn_cv = GridSearchCV(estimator(), param_grid, cv=5, scoring='f1_micro').fit(X_train, y_train)
     cv_errors = 1 - knn_cv.cv_results_["mean_test_score"]
-    std = knn_cv.cv_results_["std_test_score"]
+    # std = knn_cv.cv_results_["std_test_score"]
 
     min_ind = np.argmin(np.array(cv_errors))
     selected_k = np.array(k_range)[min_ind]
     selected_error = cv_errors[min_ind]
-
-    go.Figure([
-        go.Scatter(name='Lower CV Error CI', x=k_range, y=cv_errors - 2 * std, mode='lines',
-                   line=dict(color="lightgrey"), showlegend=False, fill=None),
-        go.Scatter(name='Upper CV Error CI', x=k_range, y=cv_errors + 2 * std, mode='lines',
-                   line=dict(color="lightgrey"), showlegend=False, fill="tonexty"),
-
-        go.Scatter(name="Train Error", x=k_range, y=train_errors, mode='markers + lines',
-                   marker_color='rgb(152,171,150)'),
-        go.Scatter(name="CV Error", x=k_range, y=cv_errors, mode='markers + lines', marker_color='rgb(220,179,144)'),
-        go.Scatter(name="Test Error", x=k_range, y=test_errors, mode='markers + lines', marker_color='rgb(25,115,132)'),
-        go.Scatter(name='Selected Model', x=[selected_k], y=[selected_error], mode='markers',
-                   marker=dict(color='darkred', symbol="x", size=10))]) \
-        .update_layout(title=r"$\text{(4) }k\text{-NN Errors - Selection By Cross-Validation}$",
-                       xaxis_title=r"$k\text{ - Number of Neighbors}$",
-                       yaxis_title=r"$\text{Error Value}$").show()
 
     return selected_k, selected_error
 
@@ -68,8 +40,8 @@ def evalute_location(y_x_predict: pd.DataFrame, y_x_true: pd.DataFrame,
     return (mean_squared_error(y_x_predict, y_x_true) + mean_squared_error(y_y_predict, y_y_true))/2
 
 
-def evalute_type(y_type_predict: pd.DataFrame, y_type_true: pd.DataFrame) -> float:
-    return
+def evaluate_type(y_type_predict: pd.DataFrame, y_type_true: pd.DataFrame) -> float:
+    return f1_score(y_type_true, y_type_predict, average="macro")
 
 
 if __name__ == '__main__':
@@ -88,8 +60,13 @@ if __name__ == '__main__':
     tel_aviv_data_for_samples = tel_aviv_data.drop(['x', 'y', 'linqmap_type', 'linqmap_subtype'], axis=1)
     X, y_type, y_subtype, y_x, y_y = make_train_data(tel_aviv_data_for_samples, tel_aviv_data)
     train_X, valid_X, train_y_type, valid_y_type, train_y_subtype, valid_y_subtype, train_y_x, valid_y_x, train_y_y, valid_y_y = train_test_split(X, y_type, y_subtype, y_x, y_y, test_size=0.2, random_state=42,  shuffle=True)
+    type_model, y_type_predict, subtypes_models = fit_types_and_subtypes(train_X, train_y_type, train_y_subtype)
 
+    ev = evaluate_type(y_type_predict, train_y_type)
+    print(f"model type score {ev}")
+    for type, model, predict_y, true_y in subtypes_models:
+        print(f"model subtype {type} score {evaluate_type(predict_y, true_y)}")
 
-    selected_k, selected_error = cross_validation(KNeighborsClassifier, train_X_flatten, y_flatten ,valid_X_flatten, valid_y_flatten, np.linspace(1, 20, 10).astype(int))
-    type_model = get_knn_model(train_X_flatten, y_flatten,valid_X_flatten, valid_y_flatten, selected_k)
+    # selected_k, selected_error = cross_validation(KNeighborsClassifier, train_X_flatten, y_flatten ,valid_X_flatten, valid_y_flatten, np.linspace(1, 20, 10).astype(int))
+    # type_model = get_knn_model(train_X_flatten, y_flatten,valid_X_flatten, valid_y_flatten, selected_k)
     print("done")
