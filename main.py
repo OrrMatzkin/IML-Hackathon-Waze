731caa6687541f9965bab784076be7b073f7ab8e
OrrMatzkin <orr.matzkin@gmail.com>
2022-06-02 10:32:18 +0300

    combined all function

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import sys
-import numpy as np
-from sklearn import model_selection
+from sklearn.model_selection import train_test_split
+import pandas as pd
 from preprocess import *
 
 if __name__ == '__main__':
     np.random.seed(0)
     raw_data = pd.read_csv("waze_data.csv")
-    train_data, test_data = model_selection.train_test_split(raw_data, test_size=.2, random_state=42)
-    ls = preprocess(train_data)
+    train_data, test_data = train_test_split(raw_data, test_size=.2, random_state=42)
+    preprocess(train_data)
     print("done")
